ac75955a40b6b629979aac6125ce034822ce3c10
Keyur Oak <keyuroak.25@gmail.com>
2022-02-18 22:18:00 +0530

    First Push

---
+++ app/src/main/java/org/co1711rem1803/coforum/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.co1711rem1803.coforum
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
