e99aabbc1b941da0bf112e6f845626573d121a5f
Deadheaven <rawoorkar30@gmail.com>
2022-02-19 11:47:02 +0530

    5th push

---
+++ app/src/main/java/org/co1711rem1803/coforum/Signup_page.kt
@@ -0,0 +1,11 @@
+package org.co1711rem1803.coforum
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Signup_page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup_page)
+    }
+}
