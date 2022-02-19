354dd467660cf2453c21d8bdee2f8e644cfd9d01
Keyur Oak <keyuroak.25@gmail.com>
2022-02-19 13:54:01 +0530

    8thPush

--- app/src/main/java/org/co1711rem1803/coforum/LogIn.kt
+++ app/src/main/java/org/co1711rem1803/coforum/LogIn.kt
@@ -1,11 +1,24 @@
 package org.co1711rem1803.coforum
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LogIn : AppCompatActivity() {
+
+    private lateinit var signUPbtn : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+
+        signUPbtn = findViewById(R.id.signUpBtn)
+
+        signUPbtn.setOnClickListener{
+            val intent = Intent(this@LogIn,Signup_page::class.java)
+            startActivity(intent)
+        }
+
     }
 }
