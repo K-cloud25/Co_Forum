12ceaa8b550882fe16e7bd5c9e8868e7aa946eb0
Keyur Oak <keyuroak.25@gmail.com>
2022-02-20 12:18:21 +0530

    10thPush

--- app/src/main/java/org/co1711rem1803/coforum/LogIn.kt
+++ app/src/main/java/org/co1711rem1803/coforum/LogIn.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 
 class LogIn : AppCompatActivity() {
 
@@ -15,36 +17,49 @@ class LogIn : AppCompatActivity() {
     private lateinit var emailBox : EditText
     private lateinit var passwordBox : EditText
 
+    private lateinit var mAuth : FirebaseAuth
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
 
+        supportActionBar?.hide() //Hides Action Bar
+
         signUPbtn = findViewById(R.id.signUpBtn)
         logInBtn = findViewById(R.id.loginBtn)
         emailBox = findViewById(R.id.emailInput)
-
+        passwordBox = findViewById(R.id.passwordInput)
+        mAuth = FirebaseAuth.getInstance()
 
         signUPbtn.setOnClickListener{
-            val intent = Intent(this@LogIn,Signup_page::class.java)
+            val intent = Intent(this@LogIn , Signup_page::class.java)
             startActivity(intent)
         }
 
         //Check Email Input For @mitwpu.edu.in  for cross checking
         logInBtn.setOnClickListener{
-            val text = emailBox.text.toString()
-            if(text.contains("@")) {
-                var check = text.split("@")
+            val emailId = emailBox.text.toString()
+            val password = passwordBox.text.toString()
 
-                if(check[1] == "mitwpu.edu.in"){
-                    Toast.makeText(this,check[1],Toast.LENGTH_LONG).show()
-                }
-                else{
-                    Toast.makeText(this,"Incorrect email ",Toast.LENGTH_LONG).show()
-                }
+            if(emailId=="" || password ==""){
+                Toast.makeText(this,"Enter Email and Password",Toast.LENGTH_SHORT).show()
             }
-            else{
-                Toast.makeText(this,"Incorrect Email Format",Toast.LENGTH_LONG).show()
+            else {
+                login(emailId, password)
             }
         }
+    }
+    //Log In Function to handle login a user
+    private fun login(email:String,password:String){
+        //Login Parameters
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this@LogIn,MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this@LogIn , "User DNE",Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
--- app/src/main/java/org/co1711rem1803/coforum/Signup_page.kt
+++ app/src/main/java/org/co1711rem1803/coforum/Signup_page.kt
@@ -1,16 +1,39 @@
 package org.co1711rem1803.coforum
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Spinner
+import android.util.Log
+import android.widget.*
+import com.google.firebase.auth.FirebaseAuth
 
 class Signup_page : AppCompatActivity() {
+
+    private lateinit var signUPbtn : Button
+
+    private lateinit var emailBox : EditText
+    private lateinit var passwordBox : EditText
+    private lateinit var fullName : EditText
+    private lateinit var userName : EditText
+
+    private lateinit var mAuth : FirebaseAuth
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup_page)
 
+        supportActionBar?.hide() //Hides action Bar
+
         val spinner: Spinner = findViewById(R.id.Year_spinner)
+
+        emailBox = findViewById(R.id.emailID)
+        passwordBox = findViewById(R.id.passwordEmail)
+        signUPbtn = findViewById(R.id.signUpBtn)
+        fullName = findViewById(R.id.FullNameInput)
+        userName = findViewById(R.id.UsernameInput)
+        mAuth = FirebaseAuth.getInstance()
+
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter.createFromResource(
             this,
@@ -22,5 +45,43 @@ class Signup_page : AppCompatActivity() {
             // Apply the adapter to the spinner
             spinner.adapter = adapter
         }
+
+        signUPbtn.setOnClickListener{
+            val email = emailBox.text.toString()
+            val password=passwordBox.text.toString()
+
+            //Code Logic to check for mitwpu.edu.in Login Only
+            if(email.contains("@")){
+                var checkString = email.split("@")
+
+                if(checkString[1]=="mitwpu.edu.in"){
+                    signUp(email,password)          //Email is made with cllg ID
+                }
+                else{
+                    Toast.makeText(this@Signup_page , "Enter College ID",Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                Toast.makeText(this@Signup_page , "Incorrect Email",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    //Funtion To Make Sign UP a User
+    private fun signUp(email:String, password:String){
+        //Sign in Instructions and Parameters
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful){
+                    //Jump to User InterAction Page
+
+                    val intent = Intent(this@Signup_page , LogIn::class.java)
+                    startActivity(intent)
+
+                } else {
+                    //Initiate Sign UP Error
+                    Toast.makeText(this@Signup_page, "SignUP Error",Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
