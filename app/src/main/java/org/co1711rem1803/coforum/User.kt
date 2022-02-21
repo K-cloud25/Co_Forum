6060af2bcfd5aaa1e93a11e718c21c575b209145
Keyur Oak <keyuroak.25@gmail.com>
2022-02-21 16:03:15 +0530

    12thPush

---
+++ app/src/main/java/org/co1711rem1803/coforum/User.kt
@@ -0,0 +1,21 @@
+package org.co1711rem1803.coforum
+
+class User {
+
+    var emailId : String? = null
+    var fullName : String? = null
+    var userName : String? = null
+    var dateOfBirth : String? = null
+    var uid : String? = null
+
+
+    constructor(){}
+
+    constructor(_emailID:String,_uid:String,_fullName:String,_userName:String,_dateOfBirth : String){
+        this.emailId = _emailID
+        this.fullName = _fullName
+        this.userName = _userName
+        this.dateOfBirth = _dateOfBirth
+        this.uid = _uid
+    }
+}
--- app/src/main/java/org/co1711rem1803/coforum/UserInfo.kt
+++ app/src/main/java/org/co1711rem1803/coforum/UserInfo.kt
@@ -4,13 +4,26 @@ import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import java.util.*
 
 class UserInfo : AppCompatActivity() {
 
+    private lateinit var userName : EditText
+
     private lateinit var datePickerBtn : Button
+    private lateinit var saveBtn : Button
+
     private lateinit var dateTextdisplay : TextView
+
+    private lateinit var realT_DB : DatabaseReference
+    private lateinit var firebaseAuth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +31,13 @@ class UserInfo : AppCompatActivity() {
 
         supportActionBar?.hide()
 
+        userName = findViewById(R.id.userName)
         datePickerBtn = findViewById(R.id.datePickerBtn)
         dateTextdisplay = findViewById(R.id.dateDisplay)
+        saveBtn = findViewById(R.id.saveBtn)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
         //Calender Object and 3 variable to store day month year
         val calender = Calendar.getInstance()
         var day = calender.get(Calendar.DAY_OF_MONTH)
@@ -33,5 +51,15 @@ class UserInfo : AppCompatActivity() {
 
             dpd.show()
         }
+
+        saveBtn.setOnClickListener {
+            val Dobstring = "$day/$month/$year"
+            addUserToDB("Email",firebaseAuth.currentUser?.uid!!,"ABC",userName.text.toString(),Dobstring)
+        }
+    }
+
+    private fun addUserToDB(_emailID:String,_uid:String,_fullName:String,_userName:String,_dateOfBirth : String){
+        realT_DB = FirebaseDatabase.getInstance().getReference()
+        realT_DB.child("user").child(_uid).setValue(User(_emailID,_uid,_fullName, _userName, _dateOfBirth))
     }
 }
